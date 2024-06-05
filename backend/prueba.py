051489c9c8ec227923cfdd609b75c5a5a5b9d7b7
RaulCasado <raulcasado969@gmail.com>
2024-06-05 18:19:58 +0200

    feat: added basic generate password

--- backend/prueba.py
+++ backend/prueba.py
@@ -1,15 +1,26 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+import secrets
+import requests
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/greet', methods=['POST'])
-def greet():
-    data = request.get_json()
-    name = data.get('name', 'World')
-    greeting = f'Hello, {name}!'
-    return jsonify({'greeting': greeting})
+@app.route('/api/random_password', methods=['POST'])
+def random_password():
+    # Generate a random password using the secrets module
+    random_password = secrets.token_urlsafe(16)
+
+    return jsonify({'password': random_password})
+
+@app.route('/request_password', methods=['POST'])
+def request_password():
+    response = requests.post('http://localhost:5000/api/random_password')
+
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify({'password': data['password']})
+    else:
+        return jsonify({'error': 'Failed to get password'})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/components/GeneratePassword.jsx
+++ frontend/src/components/GeneratePassword.jsx
@@ -1,7 +1,35 @@
+import  { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export function GeneratePassword() {
-    return (
-        <div>
-            Aquí está Password
-        </div>
-    )
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    // Fetch a random word from the Python server
+    axios.post('http://localhost:5000/request_password')
+      .then(response => {
+        setPassword(response.data.password);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  const handleButtonClick = () => {
+    // Fetch a new random word when the button is clicked
+    axios.post('http://localhost:5000/request_password')
+      .then(response => {
+        setPassword(response.data.password);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <p>Random Password: {password}</p>
+      <button onClick={handleButtonClick}>Get New Word</button>
+    </div>
+  );
 }
